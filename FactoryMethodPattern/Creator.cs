e7efb940145789b3efa09b62c4b7613bcfed9451
simutaroman <simutaroman@gmail.com>
2017-07-13 14:39:35 +0300

    FactoryMethodPattern

---
+++ FactoryMethodPattern/Creator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPattern
+{
+    public abstract class Creator
+    {
+        public abstract Product FactoryMethod();
+        public Product CallFactoryMethod()
+        {
+            var product = this.FactoryMethod();
+            return product;
+        }
+    }
+}
---
+++ FactoryMethodPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // todo: this code needs to be refactored :)
+            Creator creator = new ConcreteCreator();
+            Product p1 = creator.FactoryMethod();
+            Product p2 = creator.CallFactoryMethod();
+            Console.WriteLine("p1 = {0}, p2 = {1}", p1.GetHashCode(), p2.GetHashCode());
+
+            creator = new ConcreteCreator2();
+            p1 = creator.FactoryMethod();
+            p2 = creator.CallFactoryMethod();
+            Console.WriteLine("p1 = {0}, p2 = {1}", p1.GetHashCode(), p2.GetHashCode());
+
+            Console.ReadLine();
+        }
+
+    }
+}
