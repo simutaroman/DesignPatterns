e91ef4698832d7176723002467f39219a81c9203
simutaroman <simutaroman@gmail.com>
2017-07-20 21:18:33 +0300

    Adaper class pattern

---
+++ AdapterClassPattern/Adaptee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterClassPattern
+{
+    public class Adaptee
+    {
+        public string SpecQuery()
+        {
+            return "Special query";
+        }
+    }
+}
---
+++ AdapterClassPattern/Adapter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterClassPattern
+{
+    public class Adapter : Adaptee, ITarget
+    {
+        public void Query()
+        {
+            Console.WriteLine("Calling special query from Adaptee: {0}", SpecQuery());
+        }
+    }
+}
